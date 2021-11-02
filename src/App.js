4cf83fab75d623619d2ad62f05d4a5875cb576ea
Mario <66450569+Mariosay@users.noreply.github.com>
2021-11-02 08:31:21 -0600

    agregar proyecto

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import Header from "./component/Header";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    <p>
+          Edit <code>src/App.js</code> and save to reload.
+        
+    </p>
+    
+
+    </div>
+  );
+}
+
+export default App;
